3081ee53e09f122f704b95246c9c0bb36eaacb31
DavidWilld <103283145+DavidCondoriAguilar@users.noreply.github.com>
2024-05-03 00:36:33 -0500

    feat(fixed( githubPages ))

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
+  output: "server",
   adapter: vercel(),
-
+  site: "https://davidcondoriaguilar.github.io/PortfolioDavid/",
+  base: "https://davidcondoriaguilar.github.io/",
 });
