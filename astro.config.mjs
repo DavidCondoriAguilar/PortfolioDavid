a969982ae223191e5395c151864670b4c41f2fef
DavidWilld <103283145+DavidCondoriAguilar@users.noreply.github.com>
2024-10-06 17:40:51 -0500

    new fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
-  site: "https://davidcondoriaguilar.github.io/PortfolioDavid/",
-  base: "https://davidcondoriaguilar.github.io/",
+  adapter: vercel()
+  // site: "https://davidcondoriaguilar.github.io/PortfolioDavid/",
+  // base: "https://davidcondoriaguilar.github.io/",
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        "slide-logos": {
+          to: { transform: "translateX(calc(-100% - 2rem))" },
+        },
+      },
+      animation: {
+        "slide-logos": "slide-logos 5s linear infinite",
+      },
+
+      plugins: [],
+    },
+  },
+};
